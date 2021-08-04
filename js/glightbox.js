4828f19761c59b53f8efb7e6c1608b5bfcc9e76b
Eros <erosbardhi@gmail.com>
2021-08-04 03:54:50 +0200

    Services Section Done

---
+++ js/glightbox.js
@@ -0,0 +1,8 @@
+const lightbox = GLightbox({
+    href: "https://www.youtube.com/watch?v=Sp63h39bj7k&t=7816s",
+    type: "video",
+    source: "youtube", //vimeo, youtube or local
+    width: 900,
+    loop: true,
+    autoplayVideos: true,
+  });
